667c9b2f9900cb73ecbd56ee5edd7dd09743f332
Coffe Dahl <coffedahl@hotmail.com>
2024-05-30 20:44:18 +0200

    Basic styling (#1)
    * Created navbar component
    
    Created navbar simple navbar component and added it to the layout.
    Todo: 
    - Add icons for cart and useracount
    - Add logo image
    - Add functionality
    
    * Added logo image
    
    * Added fontawsome icons
    
    Added all the packages required to run font awsomes free icons on the website.
    
    * Finished styling on the navbar
    
    Things added/changed:
    - Added correct styling and  spacing
    - Added logo image
    - Added icons
    
    * Updated title
    
    Updated title and decription in the tabs field of the browser
    
    * landing page
    
    Created a simple landing page with a banner
    
    * Increased size of Navbar
    
    Added some extra height to the navbar for a bit more sparation for the eyes

---
+++ app/(components)/Navbar.tsx
@@ -0,0 +1,26 @@
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Image from "next/image";
+
+export default function Navbar() {
+    return (
+        <header className="flex items-center justify-between h-21">
+            <div className="flex items-center">
+                <a href="/"><Image src={'/jdtek-logo.png'} width={250} height={100} alt="JDTEK logo" /></a>
+                <nav>
+                    <ul className="flex font-semibold text-2xl space-x-5">
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/calendar">Calendar</a></li>
+                        <li><a href="/shop">Webshop</a></li>
+                        <li><a href="/about">About</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div className="flex items-center mr-10 space-x-8">
+                <button><FontAwesomeIcon icon={faUser} className="h-8" /></button>
+                <button><FontAwesomeIcon icon={faShoppingCart} className="h-8"/></button>
+            </div>
+        </header>
+    )
+}
