3493e122a293ed59c2f05e2db8cb505fbc808d81
coffedahl <coffedahl@hotmail.com>
2024-05-30 20:01:45 +0200

    Finished styling on the navbar
    Things added/changed:
    - Added correct styling and  spacing
    - Added logo image
    - Added icons

--- app/(components)/Navbar.tsx
+++ app/(components)/Navbar.tsx
@@ -1,11 +1,15 @@
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Image from "next/image";
 
-export default function Navbar(){
-    return(
-        <header className="flex items-center space-content-between">
+export default function Navbar() {
+    return (
+        <header className="flex items-center justify-between h-20">
             <div className="flex items-center">
-                <a href="/"><h1>[JDTEK logo]</h1></a>
+                <a href="/"><Image src={'/jdtek-logo.png'} width={250} height={100} alt="JDTEK logo" /></a>
                 <nav>
-                    <ul className="flex">
+                    <ul className="flex font-semibold text-2xl space-x-5">
                         <li><a href="/">Home</a></li>
                         <li><a href="/calendar">Calendar</a></li>
                         <li><a href="/shop">Webshop</a></li>
@@ -13,9 +17,9 @@ export default function Navbar(){
                     </ul>
                 </nav>
             </div>
-            <div className="flex items-center">
-                <button>[Acount icon]</button>
-                <button>[Cart icon]</button>
+            <div className="flex items-center mr-10 space-x-8">
+                <button><FontAwesomeIcon icon={faUser} className="h-8" /></button>
+                <button><FontAwesomeIcon icon={faShoppingCart} className="h-8"/></button>
             </div>
         </header>
     )
