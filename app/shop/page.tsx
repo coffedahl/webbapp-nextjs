42b0a01d31cded35c3f7ebf3b38bd84b4bca2407
coffedahl <coffedahl@hotmail.com>
2024-05-30 21:42:39 +0200

    Created weshop page
    Created the webshop page with some small header and message

---
+++ app/shop/page.tsx
@@ -0,0 +1,13 @@
+export default function Shop() {
+    return (
+        <div>
+            <section className="flex flex-col justify-center items-center">
+                <h1 className="text-3xl font-semibold">Webshop</h1>
+                <p className="text-lg">Here you can find our awsome merch and products to bring your partystyle to the next level!</p>
+            </section>
+            <section className="flex flex-wrap m-10">
+                
+            </section>
+        </div>
+    )
+}
