c0696068c371d1f9d8c81cb98bb45071bcbf0fb8
Christoffer Dahl <coffedahl@hotmail.com>
2024-05-31 10:30:13 +0200

    Added product fetch to shop
    Added an function that fetches the product data from the api for the shop page

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,21 +1,36 @@
 import ProductCard from "./(components)/ProductCard";
 
-export default function Shop() {
-    const products: Array<Product> = []
-    return (
-      <div>
-        <section className="flex flex-col justify-center items-center m-8">
-          <h1 className="text-3xl font-semibold">Webshop</h1>
-          <p className="text-lg">
-            Here you can find our awsome merch and products to bring your
-            partystyle to the next level!
-          </p>
-        </section>
-        <section className="flex flex-wrap">
-          {products.map((product, x) => (
-            <ProductCard product={product} key={x} />
-          ))}
-        </section>
-      </div>
-    );
+/**
+ * Fetches the product data from the api and passes is as a array of products
+ * @returns Array of Products
+ */
+async function getProductData() {
+  // Fetch fresh data from api
+  const res = await fetch(process.env.LOCAL_URL + "/api/products", {
+    cache: "no-store",
+  });
+  // Parse and return data
+  const data: Array<Product> = await res.json()
+  return data
+}
+
+export default async function Shop() {
+  // Get product data
+  const products: Array<Product> = await getProductData()
+  return (
+    <div>
+      <section className="flex flex-col justify-center items-center m-8">
+        <h1 className="text-3xl font-semibold">Webshop</h1>
+        <p className="text-lg">
+          Here you can find our awsome merch and products to bring your
+          partystyle to the next level!
+        </p>
+      </section>
+      <section className="flex flex-wrap">
+        {products.map((product, x) => (
+          <ProductCard product={product} key={x} />
+        ))}
+      </section>
+    </div>
+  );
 }
