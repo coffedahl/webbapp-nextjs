136cfe2c83e401941ee8ba79b108786830fbd001
coffedahl <coffedahl@hotmail.com>
2024-05-31 22:46:56 +0200

    Styling updates on productcard
    Changed styling on the product cards to make the overall clarity and styling improve.

--- app/shop/(components)/ProductCard.tsx
+++ app/shop/(components)/ProductCard.tsx
@@ -10,15 +10,15 @@ export default function ProductCard({ product }: { product: Product }) {
         height={200}
       />
       <div>
-        <h1 className="font-semibold text-lg">{product.name}</h1>
+        <h1 className="font-semibold text-xl">{product.name}</h1>
         <p>{product.description}</p>
-        <h1 className="font-bold text-xl">{product.price},-</h1>
+        <h1 className="font-bold text-3xl">{product.price},-</h1>
       </div>
       <div className="flex flex-col">
-        <a href={"/shop/" + product.id}>
-          <button className="bg-slate-400 mt-3 rounded-lg p-1">Se more</button>
+        <a href={"/shop/" + product.id} className="w-full">
+          <button className="bg-slate-400 mt-3 font-semibold rounded-lg p-1 w-full">Se more</button>
         </a>
-        <button className="bg-lime-700 mt-3 rounded-lg p-1">Add to cart</button>
+        <button className="bg-lime-700 text-white font-semibold mt-3 rounded-lg p-1">Add to cart</button>
       </div>
     </div>
   );
