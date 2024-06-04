c40c1b1073b4e9b48f4116a39c0e8ab2c2a4ce9d
coffedahl <coffedahl@hotmail.com>
2024-06-04 22:44:03 +0200

    Updated middleware to have auth on acount routes
    Updated the middleware to protect acount route. Also commented login page code and bugfixed the redirect

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,22 +1,33 @@
 'use client'
+// imports
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { FormEvent } from "react";
 
 export default function Login() {
+  // Create router object
+  const router = useRouter();
+
+  /**
+   * Function for handling submit of the form and logging in user
+   */
   async function onSubmit(event: FormEvent<HTMLFormElement>) {
+    // Prevent default html functions
     event.preventDefault();
+    // Get data from the form
     const formData = new FormData(event.currentTarget);
     const email = formData.get("email") as string;
     const password = formData.get("password") as string;
 
+    // Log in user with supabase
     const supabase = createClientComponentClient();
     const { data, error } = await supabase.auth.signInWithPassword({
       email,
       password,
     });
-    if(data.user){
-      redirect("/");
+    if (data.user) {
+      // if login was successful go back to start page
+      router.push("/");
     }
   }
 
--- middleware.ts
+++ middleware.ts
@@ -4,12 +4,20 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export async function middleware(req: NextRequest) {
-  const res = NextResponse.next()
+  const res = NextResponse.next();
 
-  const supabase = createMiddlewareClient({ req, res })
+  const supabase = createMiddlewareClient({ req, res });
 
   // Refresh session if expired - required for Server Components
-  await supabase.auth.getSession()
+  await supabase.auth.getSession();
 
-  return res
+  // Get user from session
+  const user = await supabase.auth.getUser();
+
+  // If user tries to access acount route and is not logged in
+  if (req.nextUrl.pathname.startsWith("/acount") && user.error) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  return res;
 }
